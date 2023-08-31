3c9b0b967faf2d4bcc279fb40d26f4ab72ad49ec
lgramajo99 <lucianongramajo@gmail.com>
2023-08-31 18:40:39 -0300

    Estructura de Home Listo

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import Bar from "./components/Bar"
 // import AboutMe from "./pages/AboutMe"
+import Home from "./pages/Home"
 // import TabBar from "./components/TabBar"
 
 function App() {
   return (
     <div >
       <Bar />
-      <section >
-
+      <section className="p-10">
+        <Home />
       </section>
     </div>
   )
---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+function Home() {
+    const unavatarUrl: string = 'https://unavatar.io/github/lgramajo99';
+    return (
+        <div>
+            <h1>DESARROLLADOR DE SOFTWARE</h1>
+            <h2>LUCIANO GRAMAJO</h2>
+
+            <img src={unavatarUrl} alt="Foto de perfil de Luciano Gramajo" />
+        </div>
+    )
+}
+export default Home;

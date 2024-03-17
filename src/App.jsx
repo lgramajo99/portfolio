1072649b45c12bf8b2c0468686a3cd275273e42f
lgramajo99 <lucianongramajo@gmail.com>
2024-03-17 16:44:23 -0300

    Empezando el proyecto portfolio

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+
+  return (
+    <main>
+      <h1>PORTFOLIO</h1>
+    </main>
+  )
+}
+
+export default App

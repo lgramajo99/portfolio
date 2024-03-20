de587165f91039f944e01dc73d45c887f46e9e39
lgramajo99 <lucianongramajo@gmail.com>
2024-03-20 01:09:20 -0300

    listo el footer, falta logica de cambios de tema y lenguaje

--- src/components/layout/footer/Footer.jsx
+++ src/components/layout/footer/Footer.jsx
@@ -6,38 +6,35 @@ function Footer() {
     return (
         <footer className='footer'>
             <section className='changes'>
-                <button type='button' className='footer-button'>
+                <button type='button' className='footer-button' title='Cambiar theme mode '>
                     <img src={moonLigth && brightness} className='icon' alt="Dark or light mode" />
                 </button>
 
-                <button type='button' className='footer-button'>
-                    <span>
-                        <img className='icon' src={dictionarylanguage} alt="ES/EN" />
-                    </span>
+                <button type='button' className='footer-button' title='Español / English'>
+                    <img className='icon' src={dictionarylanguage} alt="ES/EN" />
                 </button>
             </section>
 
             <section className='social'>
                 <hr />
-                <a href="#" rel="noopener noreferrer">
+                <a href="#" rel="noopener noreferrer" title='Ir a dibbble'>
                     <img src={dribbble} className='icon' alt="dribbble" />
                 </a>
 
                 <hr />
-                <a href="https://www.linkedin.com/in/lucianogramajo/" rel="noopener noreferrer">
+                <a href="https://www.linkedin.com/in/lucianogramajo/" rel="noopener noreferrer" title='Ir a linkedIn'>
                     <img src={linkedin} className='icon' alt="linkedin" />
                 </a>
 
                 <hr />
-                <a href="https://github.com/lgramajo99/" target='_blank' rel="noopener noreferrer">
+                <a href="https://github.com/lgramajo99/" target='_blank' rel="noopener noreferrer" title='Ir a github'>
                     <img src={github} className='icon' alt="github" />
                 </a>
 
                 <hr />
-                <a href="#" target='_blank' rel="noopener noreferrer">
+                <a href="#" target='_blank' rel="noopener noreferrer" title='Ir a paypal'>
                     <img src={paypal} className='icon' alt="paypal" />
                 </a>
-
                 <hr />
             </section>
             <span></span>

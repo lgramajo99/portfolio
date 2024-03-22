4e8384a0461e9f95e7d9ec8ad715bf0e951ca9c8
lgramajo99 <lucianongramajo@gmail.com>
2024-03-22 20:41:56 -0300

    remodelando el componente Skills.

--- src/page/Skills.jsx
+++ src/page/Skills.jsx
@@ -1,5 +1,59 @@
 function Skills() {
-    return (<main><h2>Habilidades</h2></main>)
+    return (
+        <main>
+            <h2>Habilidades</h2>
+            <div className="habilidades-container">
+                <div className="lenguajes">
+                    <h3>Lenguajes de Programación</h3>
+                    <ul>
+                        <li>JavaScript</li>
+                        <li>Type-Script</li>
+                        <li>Java</li>
+                        <li>Kotlin</li>
+
+                    </ul>
+                </div>
+                <div className="frameworks">
+                    <h3>Frameworks y Librerías</h3>
+                    <ul>
+                        <li>React.js</li>
+                        <li>Node.js</li>
+                        <li>Express.js</li>
+                    </ul>
+                </div>
+                <div className="tools">
+                    <h3>Herramientas de Desarrollo</h3>
+                    <ul>
+                        <li>Visual Studio Code</li>
+                        <li>Git</li>
+                        <li>Docker</li>
+                    </ul>
+                </div>
+                <div className="methodologies">
+                    <h3>Metodologías y Procesos</h3>
+                    <ul>
+                        <li>Scrum</li>
+                        <li>Kanban</li>
+                        <li>Metodología en Cascada (Waterfall)</li>
+                    </ul>
+                </div>
+                <div className="interpersonal skills">
+                    <h3>Habilidades Interpersonales</h3>
+                    <ul>
+                        <li>Comunicación efectiva</li>
+                        <li>Trabajo en equipo</li>
+                        <li>Liderazgo</li>
+                    </ul>
+                </div>
+                <div className="certifications">
+                    <h3>Certificaciones y Acreditaciones</h3>
+                    <ul>
+                        <li>Certificado en Desarrollo Web Full Stack</li>
+                        <li>Certificado en Scrum Master</li>
+                    </ul>
+                </div>
+            </div>
+        </main>)
 }
 
 export default Skills
